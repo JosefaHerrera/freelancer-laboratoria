a4ce349e5c03f326c7358ea191246f1f47de8efb
JosefaHerrera <josefa@linuxero.cl>
2017-06-01 10:38:02 -0400

    empezando validaciones

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
  * Archivo principal de JS
  */
 
-// Array.from para que reconozca la clase como un arreglo
+
 /*MODAL EJEMPLO BLANCA*/
 (function main() {
+	// Array.from para que reconozca la clase como un arreglo
     var boxes = Array.from(document.getElementsByClassName('col-4'));
     var modal = document.getElementById('col-4-modal');
     var bodyModal, close, img;
@@ -42,3 +43,54 @@
 
 
 })();
+
+
+
+/********VALIDACIONES*********/
+
+
+//validar nombre
+		function validarNombre(){
+		var nombre = document.getElementById('nombrecito').value; //valor ingresado al input en el id='name'
+		     //validar que nombre no esté vacío
+		if(nombre === "" || nombre === undefined || nombre.length === 0){
+			alert("Error, debe ingresar nombre");
+			//validar que 1° caracter sea mayúscula
+		}else if(nombre.charAt(0) != nombre.charAt(0).toUpperCase()){
+			alert("Error, el nombre debe comenzar con mayúscula");
+			//validar que sean solo letras
+		}else if(/[0-9]/.test(nombre)){
+			alert("Error, no debe ingresar números")
+		}else{
+			console.log("Nombre ingresado correctamente")
+		}
+	}
+	nombre();
+
+
+//validar correo
+
+	function validarCorreo(){
+        var correo = document.getElementById('input-email').value;
+        if(correo === null || correo.length === 0 || /^\s+$/.test(correo)){
+            alert("campo obligatorio");
+        }else if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(correo)){
+            alert("Debe ingresar un Email válido");      
+        }
+    };
+    validarCorreo();
+
+//validar Telefono
+function validarPhone(){
+	var num=document.getElementById("phone").value;
+	if(num.length!=9){
+		alert("Debe ingresar un número celular de 9 dígitos");
+	}else{
+		var text=document.getElementById("phone");
+		alert("Numero ingresado");
+	}
+};
+validarPhone();
+
+
+
